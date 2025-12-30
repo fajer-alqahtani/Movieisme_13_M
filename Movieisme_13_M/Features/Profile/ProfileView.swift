8a96ef74dcde9038fca3cb92fa7b8a92a662e647
asma <as_xx2@icloud.com>
2025-12-30 04:40:09 +0300

    the profile and sign in

--- Movieisme_13_M/Features/Profile/ProfileView.swift
+++ Movieisme_13_M/Features/Profile/ProfileView.swift
@@ -5,3 +5,102 @@
 //  Created by Yousra Abdelrahman on 04/07/1447 AH.
 //
 
+
+
+import SwiftUI
+
+struct ProfileView: View {
+    @State var savedMovies: [String] = []
+    @State var movies: [String] = []
+    var body: some View {
+        
+        
+    VStack(alignment: . leading, spacing: 20) {
+        
+        HStack{
+            Image(systemName: ("arrow.left"))
+                .font(.system(size: 25))
+                .foregroundColor(.yellow)
+            Text("Back")
+                .font(.system(size:20))
+                .foregroundColor(.yellow)
+        }
+            Text("Profile")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+
+        
+    
+        
+            ZStack{
+                
+        Rectangle()
+        .frame(width: 390, height: 80)
+        .foregroundColor(.gray.opacity(0.2))
+            .cornerRadius(10)
+                
+                HStack() {
+                    Image("avatar")
+            
+                  
+    VStack(alignment : .leading , spacing: 5){
+                        Text("Sarah Abdullah")
+                .font(.system(size:18))
+            .font(.headline)
+                .fontWeight(.bold)
+                    .foregroundColor(.white)
+                Text("Xxxx234@gmail.com")
+                .foregroundColor(.white)
+                .tint(.white)
+                .font(.system(size:12))
+                
+                    
+               
+    }
+                    Spacer()
+                    
+                    
+                    Image(systemName: ("chevron.right"))
+                
+                }
+               
+                .padding(.horizontal , 16)
+            }
+        Text("Saved movies")
+            .font(.system(size:22))
+            .fontWeight(.bold)
+            
+            
+                
+        Spacer()
+                
+        VStack{
+            if movies.isEmpty {
+                // شاشة فاضية
+            } else {
+                // عرض الأفلام
+            }
+        }
+            
+                    
+                }
+  
+                        
+                    
+        
+                    
+               
+        
+            
+            
+            
+            
+            
+            
+            
+        }}
+#Preview {
+    ProfileView()
+        .preferredColorScheme(.dark)
+}
--- Movieisme_13_M/Features/SignIn/SignInView.swift
+++ Movieisme_13_M/Features/SignIn/SignInView.swift
@@ -1,7 +1,89 @@
-//
-//  SignInView.swift
-//  Movieisme_13_M
-//
-//  Created by Yousra Abdelrahman on 04/07/1447 AH.
-//
+import SwiftUI
 
+struct SignInView: View {
+    
+    @State private var email: String = ""
+    @State private var password: String = ""
+    
+    var body: some View {
+        VStack {
+            
+            ZStack {
+                
+                Image("background")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .edgesIgnoringSafeArea(.all)
+                    .frame(height: 1200)
+                
+                LinearGradient(
+                    gradient: Gradient(colors: [Color.black,
+                                                Color.black.opacity(0.3)]),
+                    startPoint: .bottom,
+                    endPoint: .top
+                )
+                
+                VStack(spacing: 10) {
+                    
+                    Text("Sign in")
+                        .font(.system(size: 40))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .padding(.trailing, 200)
+                    
+                    Text("You'll find what you're looking for in the ocean of movies")
+                        .frame(width: 318)
+                        .lineLimit(2)
+                        .font(.system(size: 18))
+                        .foregroundColor(.white)
+                    
+                    Text("Email")
+                        .font(.system(size: 18))
+                        .foregroundColor(.white)
+                        .padding(.trailing, 260)
+                        .padding(.vertical, 10)
+                    
+                    TextField("Enter your email", text: $email)
+                        .frame(width: 358, height: 44)
+                        .foregroundColor(.white)
+
+                        .background(Color.black.opacity(0.5))
+                        .padding(.horizontal, 12)
+                        .cornerRadius(10)
+                        .textInputAutocapitalization(.never)
+                        .keyboardType(.emailAddress)
+                    
+                    Text("Password")
+                        .font(.system(size: 18))
+                        .foregroundColor(.white)
+                        .padding(.trailing, 260)
+                        .padding(.vertical, 10)
+                    
+                    SecureField("Enter your password", text: $password)
+                        .frame(width: 358, height: 44)
+                        .foregroundColor(.white)
+                        .padding(.horizontal, 12)    .background(Color.black.opacity(0.5))
+                        .cornerRadius(10)
+                    
+                    Button {
+                    } label: {
+                        Text("Sign in")
+                            .font(.system(size: 18))
+                            .fontWeight(.bold)
+                            .foregroundColor(.gray)
+                            .frame(width: 358, height: 44)
+                            .background(Color.white)
+                            .cornerRadius(10)
+                    }
+                    .padding()
+                }
+                .padding(.top, 360)
+            }
+           
+        }
+    }
+}
+
+#Preview {
+    SignInView()
+}
